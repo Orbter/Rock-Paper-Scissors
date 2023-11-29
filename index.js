0fef4729329f7f8e091caa7165281559364a1448
Orbter <assouline39@gmail.com>
2023-11-29 21:29:14 +0200

    prototype of the finish js for the rock game

--- index.js
+++ index.js
@@ -10,96 +10,94 @@ function getComputerChoice() {
   }
   return computerChoice;
 }
+function changepicture(url) {
+  myImg = document.getElementById("question-mark-player");
+  myImg.src = url;
+}
+function changepicture_computer(url) {
+  myImg = document.getElementById("question-mark-computer");
+  myImg.src = url;
+}
 
 let playerScore = 0;
 let computerScore = 0;
 const roundTowin = 3;
 let round = 1;
-
+let newImg;
+let myImg;
 function playRound(playerMove) {
   let x = playerMove;
   let y = getComputerChoice();
+
   console.log(`round ${round}`);
   if (x === "Rock") {
+    changepicture("./images/meteore.png");
     if (y === "Rock") {
       document.querySelector(".match-resoult").textContent = `It's a tie!`;
       document.querySelector(
         ".match-contex"
       ).textContent = `you played ${x} and the bot played ${y}`;
-      document.querySelector(".players-imgchoice").innerHTML =
-        '<img src ="./images/meteore.png">';
-      document.querySelector(".choice-computer").innerHTML =
-        '<img src ="./images/meteore.png">';
+      changepicture_computer("./images/meteore.png");
     } else if (y === "Paper") {
       computerScore++;
       document.querySelector(".match-resoult").textContent = `You lost!`;
       document.querySelector(
         ".match-contex"
       ).textContent = `you played ${x} and the bot played ${y}`;
-      document.querySelector(".players-imgchoice").innerHTML =
-        '<img src ="./images/meteore.png">';
-      document.querySelector(".choice-computer").innerHTML =
-        '<img src ="./images/hologram.png">';
+      changepicture_computer("./images/hologram.png");
     } else {
       playerScore++;
       document.querySelector(".match-resoult").textContent = `You win!`;
       document.querySelector(
         ".match-contex"
       ).textContent = `you played ${x} and the bot played ${y}`;
-      document.querySelector(".players-imgchoice").innerHTML =
-        '<img src ="./images/meteore.png">';
-      document.querySelector(".choice-computer").innerHTML =
-        '<img src ="./images/lightsaber.png">';
+      changepicture_computer("./images/lightsaber.png");
     }
   } else if (x === "Paper") {
+    changepicture("./images/hologram.png");
     if (y === "Rock") {
       playerScore++;
       document.querySelector(".match-resoult").textContent = `You win!`;
       document.querySelector(
         ".match-contex"
       ).textContent = `you played ${x} and the bot played ${y}`;
-      document.querySelector(".choice-humen").innerHTML =
-        '<img src ="./images/hologram.png">';
+      changepicture_computer("./images/meteore.png");
     } else if (y === "Paper") {
       document.querySelector(".match-resoult").textContent = `It's a tie!`;
       document.querySelector(
         ".match-contex"
       ).textContent = `you played ${x} and the bot played ${y}`;
-      document.querySelector(".choice-humen").innerHTML =
-        '<img src ="./images/hologram.png">';
+      changepicture_computer("./images/hologram.png");
     } else {
       computerScore++;
       document.querySelector(".match-resoult").textContent = `You lost!`;
       document.querySelector(
         ".match-contex"
       ).textContent = `you played ${x} and the bot played ${y}`;
-      document.querySelector(".choice-humen").innerHTML =
-        '<img src ="./images/hologram.png">';
+      changepicture_computer("./images/lightsaber.png");
     }
   } else {
+    changepicture("./images/lightsaber.png");
     if (y === "Rock") {
       computerScore++;
       document.querySelector(".match-resoult").textContent = `You lost!`;
       document.querySelector(
         ".match-contex"
       ).textContent = `you played ${x} and the bot played ${y}`;
-      document.querySelector(".choice-humen").innerHTML =
-        '<img src ="./images/lightsaber.png">';
+      changepicture_computer("./images/meteore.png");
     } else if (y === "Paper") {
       playerScore++;
       document.querySelector(".match-resoult").textContent = `You win!`;
       document.querySelector(
         ".match-contex"
       ).textContent = `you played ${x} and the bot played ${y}`;
-      document.querySelector(".choice-humen").innerHTML =
-        '<img src ="./images/lightsaber.png">';
+      changepicture_computer("./images/hologram.png");
     } else {
       document.querySelector(".match-resoult").textContent = `It's a tie!`;
       document.querySelector(
         ".match-contex"
       ).textContent = `you played ${x} and the bot played ${y}`;
-      document.querySelector(".choice-humen").innerHTML =
-        '<img src ="./images/lightsaber.png">';
+      changepicture_computer("./images/lightsaber.png");
     }
   }
   document.querySelector(
